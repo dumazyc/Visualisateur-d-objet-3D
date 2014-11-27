ca26fdb1dbc4ffc8ed17be9b51217a3700a2aeb8
sk7ech <sky_tech@live.fr>
2014-11-27 22:33:36 +0100

    BDD 2

---
+++ SQLite/src/SQLite.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class SQLite
+{
+  public static void main( String args[] )
+  {
+    Connection c = null;
+    Statement stmt = null;
+    try {
+      Class.forName("org.sqlite.JDBC");
+      c = DriverManager.getConnection("jdbc:sqlite:Database.db");
+      System.out.println("Opened database successfully");
+
+      stmt = c.createStatement();
+      String sql = "CREATE TABLE OBJETS3D " +
+                   "(ID serial PRIMARY KEY     NOT NULL," +
+                   " NAME           TEXT    NOT NULL, " + 
+                   " FORME          TEXT    NOT NULL, " + 
+                   " UTILISATION    TEXT    NOT NULL, " + 
+                   " VOLUME         TEXT    NOT NULL, " + 
+                   " DATECREATION   DATETIME    NOT NULL, " + 
+                   " COMPLEXITE     INT    	NOT NULL," +
+                   " LIEN 			TEXT 	NOT NULL)"; 
+      stmt.executeUpdate(sql);
+      stmt.close();
+      c.close();
+    } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+    System.out.println("Table created successfully");
+  }
+}
