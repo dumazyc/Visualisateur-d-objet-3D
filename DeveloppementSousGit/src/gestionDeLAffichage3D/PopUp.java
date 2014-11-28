41cb72b52bcc93281cc674a22bd5978c5c64f777
daminou <daminou@daminou-VPCEC1M1E>
2014-11-28 18:59:16 +0100

    Ajout de la classe popUp.

---
+++ DeveloppementSousGit/src/gestionDeLAffichage3D/PopUp.java
@@ -0,0 +1,13 @@
+package gestionDeLAffichage3D;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class PopUp extends JFrame {
+
+	public PopUp() {
+	JOptionPane jop3;
+	jop3 = new JOptionPane();
+	jop3.showMessageDialog(null, "Message d'erreur : Le cheveux de clement ne sont pas blond !!!", "Erreur", JOptionPane.ERROR_MESSAGE);
+	}
+}
