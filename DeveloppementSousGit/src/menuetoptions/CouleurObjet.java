42f79204b3d37a38de07b887bd86742b13eead50
Clément <clement.dumazy@gmail.com>
2014-12-21 16:29:16 +0100

    Le changement des couleurs est effectué immédiatement. (plus besoin de bouger la figure pour voir le changement)

--- DeveloppementSousGit/src/menuetoptions/CouleurObjet.java
+++ DeveloppementSousGit/src/menuetoptions/CouleurObjet.java
@@ -1,19 +1,20 @@
 package menuetoptions;
 
-import gestiondelaffichage3d.AffichageDuModele;
+import gestiondelaffichage3d.PanelAffichage;
 
 import java.awt.Color;
 
 import javax.swing.JColorChooser;
 
-public class CouleurObjet{
-	AffichageDuModele a;
-	public static Color couleur = new Color(255,112,0);
+public class CouleurObjet {
+	public static Color couleur = new Color(255, 112, 0);
 
-	public CouleurObjet(AffichageDuModele a){
-		couleur = JColorChooser.showDialog(null, "Choisir une couleur pour l'objet", couleur);
-        if(couleur == null){
-            couleur = Color.WHITE;
-        }
+	public CouleurObjet(PanelAffichage p) {
+		couleur = JColorChooser.showDialog(null,
+				"Choisir une couleur pour l'objet", couleur);
+		if (couleur == null) {
+			couleur = Color.WHITE;
+		}
+		p.repaint();
 	}
 }
