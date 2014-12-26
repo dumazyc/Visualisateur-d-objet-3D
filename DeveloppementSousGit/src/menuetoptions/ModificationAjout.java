3ed922605af2aebf758c3d237d8fbc65ba440588
mkiam <migan_karen@yahoo.fr>
2014-12-26 12:04:52 +0100

    possibilite de modif

--- DeveloppementSousGit/src/menuetoptions/ModificationAjout.java
+++ DeveloppementSousGit/src/menuetoptions/ModificationAjout.java
@@ -1,7 +1,5 @@
 package menuetoptions;
 
-import gestiondelaffichage3d.AffichageDuModele;
-
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -9,14 +7,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-import java.util.ArrayList;
-
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -25,9 +18,15 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+//Classe pour modifier les informations relatives a un objet ajoute
+
 public class ModificationAjout {
+
+	//objet a modifier et fenetre
 	String nomObjet;
 	final JFrame frame = new JFrame("Veuillez ajouter les informations a modifier:   ");
+
+	//constructeur
 	public ModificationAjout(final String nomObjet){
 		this.nomObjet=nomObjet;
 		Dimension d=new Dimension(100,27);
@@ -35,7 +34,9 @@ public class ModificationAjout {
 		c.setLayout(new GridLayout(8,1,7,7));
 
 
-		c.add(new JLabel("  VEUILLEZ ENTRER LES INFORMATIONS RELATIVES A VOTRE OBJET:     "));
+		c.add(new JLabel("  VEUILLEZ ENTRER LES INFORMATIONS RELATIVES A VOTRE OBJET:    "));
+
+		//gestion des checkbox
 
 		final JPanel contentPane = new JPanel();
 
@@ -90,109 +91,70 @@ public class ModificationAjout {
 		c.add(contentPane2);
 		JPanel pa1=new JPanel();
 		final JButton valider=new JButton("Valider ");
+
+		//quand on valide
 		valider.addActionListener(new ActionListener() {
-			@SuppressWarnings("null")
 			public void actionPerformed(ActionEvent e) {
+
 				//tester ce qui a ete coche
-				if(!cbox1.isSelected()||!cbox.isSelected()){
+
+				if((cbox1.isSelected()&&tfield1.getText().isEmpty())||(cbox.isSelected()&&tfield.getText().isEmpty())){
 					JOptionPane.showMessageDialog(frame,
-							"Tous les champs sont obligatoire",
+							"Un champ coche ne peut etre vide.",
 							"Attention",
 							JOptionPane.WARNING_MESSAGE);
-
 				}
-				else
-					if((cbox1.isSelected()&&tfield1.getText().isEmpty())||(cbox.isSelected()&&tfield.getText().isEmpty())){
+				else{
+					String sql = "";
+					String name;
+					String auteur;
+					name = tfield.getText();
+					auteur = tfield1.getText();
+
+					//on interroge la base de donnees
+					if(cbox.isSelected()&&!tfield.getText().equals(null))
+						sql ="UPDATE OBJETS3D  SET NAME ="+ "'"+ name +"'" + "WHERE NAME =" + "'"+ ModificationAjout.this.nomObjet +"';";
+
+
+					if(cbox1.isSelected()&&!tfield1.getText().equals(null))
+						sql ="UPDATE OBJETS3D  SET AUTEUR ="+ "'"+ auteur +"'" + "WHERE NAME =" + "'"+ ModificationAjout.this.nomObjet +"';";
+
+					if(cbox1.isSelected()&&!tfield1.getText().equals(null)&&cbox.isSelected()&&!tfield.getText().equals(null)){
+
+						sql ="UPDATE OBJETS3D  SET NAME ="+ "'"+ name +"'" + ", AUTEUR ="+ "'"+ auteur +"'" + "WHERE NAME =" + "'"+ nomObjet +"';";
+
+					}
+
+
+
+					Connection c = null;
+					Statement stmt = null;
+					try {
+						Class.forName("org.sqlite.JDBC");
+						c = DriverManager.getConnection("jdbc:sqlite:Database.db");
+						c.setAutoCommit(false);
+						stmt = c.createStatement();
+						stmt.executeUpdate(sql);
+						c.commit();
+
+						//pour confirmer 
+						//custom title, no icon
 						JOptionPane.showMessageDialog(frame,
-								"Un champ coche ne peut etre vide.",
-								"Attention",
-								JOptionPane.WARNING_MESSAGE);
+								"Modification bien prise en compte",
+								"A plain message",
+								JOptionPane.PLAIN_MESSAGE);
+
+						stmt.close(); 
+						c.commit();
+						c.close();
+
+						//en cas d'erreur
+					} catch ( Exception e1 ) {
+						System.err.println( e1.getClass().getName() + ": " + e1.getMessage() );
+						System.exit(0);
+
 					}
-					else{
-						System.out.println("coucou");
-
-						String sql = "";
-						String name;
-						String auteur;
-						name = tfield.getText();
-						auteur = tfield1.getText();
-
-						if(cbox.isSelected()&&!tfield.getText().equals(null))
-							sql ="UPDATE OBJETS3D  SET NAME ="+ "'"+ name +"'" + "WHERE NAME =" + "'"+ ModificationAjout.this.nomObjet +"';";
-
-
-						if(cbox1.isSelected()&&!tfield1.getText().equals(null))
-							sql ="UPDATE OBJETS3D  SET AUTEUR ="+ "'"+ auteur +"'" + "WHERE NAME =" + "'"+ ModificationAjout.this.nomObjet +"';";
-
-						if(cbox1.isSelected()&&!tfield1.getText().equals(null)&&cbox.isSelected()&&!tfield.getText().equals(null)){
-							//UPDATE `membres` SET `email`="jacques@monfai.fr", `pseudo`="Jacques" WHERE `id`=1;
-							sql ="UPDATE OBJETS3D  SET NAME ="+ "'"+ name +"'" + ", AUTEUR ="+ "'"+ auteur +"'" + "WHERE NAME =" + "'"+ nomObjet +"';";
-							System.out.println(sql);
-							//sql ="UPDATE OBJETS3D  SET NAME = '"+name+"' WHERE condition"
-						}
-
-
-
-						Connection c = null;
-						Statement stmt = null;
-						try {
-							Class.forName("org.sqlite.JDBC");
-							c = DriverManager.getConnection("jdbc:sqlite:Database.db");
-							c.setAutoCommit(false);
-							stmt = c.createStatement();
-							//String sql1="INSERT INTO OBJETS3D (NAME,AUTEUR) " +
-							//	"VALUES ('"+name+"', '"+auteur+"' );";
-
-
-
-							//nous avons besoin de l'objet pour recuperer sa complexite il s'agira d'un objet qu'on dont-on a teste l'appartenance a la communaute gts
-							//complexite a modifier lors de l'insertion appeler la methode getface size sur le fichier charge!!!!!!!!!!!!!
-
-
-							stmt.executeUpdate(sql);
-							c.commit();
-							Object[] options = {" Ok,Visualiser Objet",
-							"Modifier les informations"};
-
-							@SuppressWarnings("unused")
-							final JOptionPane optionPane=new JOptionPane();
-
-							int n = JOptionPane.showOptionDialog(frame,
-									"Objet bien ajoute",
-									"",
-									JOptionPane.YES_NO_OPTION,
-									JOptionPane.QUESTION_MESSAGE,
-									null,     //do not use a custom Icon
-									options,  //the titles of buttons
-									options[0]); //default button title
-
-							if (n == JOptionPane.YES_OPTION) {
-								frame.dispose();
-								AffichageDuModele aff = null;
-								aff.nouvelOnglet(ModificationAjout.this.nomObjet);
-
-
-							} else if (n== JOptionPane.NO_OPTION) {
-								frame.dispose();
-								/*nouvelle fenetre ajout avec tous ce qu'il y a dans ajout sans le jtextfield et dont les info serviront 
-		a modifier l'objet dans la base de donnees vendredi*/ 
-								new ModificationAjout(ModificationAjout.this.nomObjet);
-							}
-							
-
-
-
-
-							stmt.close(); 
-							c.commit();
-							c.close();
-						} catch ( Exception e1 ) {
-							System.err.println( e1.getClass().getName() + ": " + e1.getMessage() );
-							System.exit(0);
-
-
-						}
-					}
+				}
 			}
 
 
@@ -212,7 +174,7 @@ public class ModificationAjout {
 		pa2.add(annuler);
 		c.add(pa2);
 
-		// ferme l'application
+		// ferme la fenetre
 		annuler.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame.dispose();
