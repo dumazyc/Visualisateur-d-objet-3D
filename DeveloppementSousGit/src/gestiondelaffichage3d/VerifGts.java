948963f933e62f5735ff800e97e56c029533bb93
damien-lepeltier <damien.lepeltier@etudiant.univ-lille1.fr>
2015-01-04 18:04:27 +0100

    classe Verifcation du GTS faite, utiliser la méthode boolean public GtsEstCorrect() pour savoir si le gts est corect ou non. Je vais opti cette classe en utilisant la classe recupDonnéesFichier.

--- DeveloppementSousGit/src/gestiondelaffichage3d/VerifGts.java
+++ DeveloppementSousGit/src/gestiondelaffichage3d/VerifGts.java
@@ -5,13 +5,23 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
+import org.ibex.nestedvm.UnixRuntime.ForkedProcess;
+
+// classe qui permet de verifier si un point gts est corecte.
+// pour savoir si il est correct, il faut utiliser la methode public GtsEstCorrect() qui return true si il est correct
 public class VerifGts {
 
 	
 	private List<Point> list_points = new ArrayList<Point>();
 	private List<Segment> list_segments = new ArrayList<Segment>();
 	private List<Face> list_faces = new ArrayList<Face>();
+	private int nbPoints;
+	private int nbSegments;
+	private int nbFaces;
+
+	
 	
 	
 	public VerifGts(String name){
@@ -24,9 +34,9 @@ public class VerifGts {
 	}
 	
 	
-	int nbPoints = s.nextInt();
-	int nbSegments = s.nextInt();
-	int nbFaces = s.nextInt();
+	nbPoints = s.nextInt();
+	nbSegments = s.nextInt();
+	nbFaces = s.nextInt();
 	int parcoursDeLigne = 1;
 	for (int i = parcoursDeLigne; i < parcoursDeLigne+nbPoints; i++) {
 			list_points.add(new Point(Double.parseDouble(s.next()),Double.parseDouble(s.next()),Double.parseDouble(s.next()),i));		
@@ -40,6 +50,117 @@ public class VerifGts {
 		
 		list_faces.add(new Face(list_segments.get(s.nextInt()-1), list_segments.get(s.nextInt()-1), list_segments.get(s.nextInt()-1), i));
 	}
+	
+	
 }
 	
+	// verifie que pour chaques segments a bien des points qui existe
+	private boolean segment_pointOK(){
+		
+
+		Point segmentP1;
+		Point segmentP2;
+		int nb =0;
+		int nb2 =0;
+		
+		
+		for (int i = 0; i<nbSegments-1 ; i++) {
+			segmentP1 =list_segments.get(i).getP1();
+			segmentP2 =list_segments.get(i).getP2();
+			
+			for (int j = 0; j < nbPoints-1; j++) {
+				if(segmentP1.equals(list_points.get(j))){
+					nb++;				
+				}
+				if(segmentP2.equals(list_points.get(j))){
+					nb2++;
+				}
+			}
+			if(nb==0 || nb2 == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	// verifie que pour chaques faces que les segments existe
+	private boolean face_segmentOk(){
+		
+		Segment faceS1;
+		Segment faceS2;
+		Segment faceS3;
+		int nb =0;
+		int nb2=0;
+		int nb3=0;
+		
+		for (int i = 0; i < nbFaces-1; i++) {
+			faceS1=list_faces.get(i).getS1();
+			faceS2=list_faces.get(i).getS2();
+			faceS3=list_faces.get(i).getS3();
+			
+			for (int j = 0; j < nbSegments-1; j++) {
+				if (faceS1.equals(list_segments.get(j))) {
+					nb++;
+				}
+				if (faceS2.equals(list_segments.get(j))) {
+					nb2++;
+				}
+				if (faceS3.equals(list_segments.get(j))) {
+					nb3++;
+				}
+			}		
+		}
+		if(nb==0 || nb2 ==0 || nb3==0){
+			return false;
+		}
+		return true;
+	}
+	
+	// verifie que pour chaques faces les trois segment ne sont pas identique 
+	private boolean facePasMemeSegmentok(){
+		
+		Segment faceS1;
+		Segment faceS2;
+		Segment faceS3;
+		int nb =0;
+		int nb2=0;
+		int nb3=0;
+		
+		for (int i = 0; i < nbFaces-1; i++) {
+			faceS1=list_faces.get(i).getS1();
+			faceS2=list_faces.get(i).getS2();
+			faceS3=list_faces.get(i).getS3();
+			
+			if(faceS1.equals(faceS3) || faceS1.equals(faceS3) || faceS2.equals(faceS3)){
+				return false;		
+			}			
+		}	
+		return true;
+	}
+	
+	// verifie que pour chaques segments les deux points ne sont pas identique 
+	private boolean segmentPasMemePoint(){
+
+		Point segmentP1;
+		Point segmentP2;
+		int nb =0;
+		
+		for (int i = 0; i<nbSegments-1 ; i++) {
+			segmentP1 =list_segments.get(i).getP1();
+			segmentP2 =list_segments.get(i).getP2();
+			
+			if(segmentP1.equals(segmentP2)){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	// return true si le point gts est correct sinon false.
+	public boolean GtsEstCorrect(){
+		if(segment_pointOK() && segmentPasMemePoint() && face_segmentOk() && facePasMemeSegmentok()){
+			return true;
+		}
+		else{return false;}
+	}
 }
