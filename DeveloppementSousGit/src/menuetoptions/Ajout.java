bf7c370ee44a7c726c6c63646ef624f1da180bb6
mkiam <migan_karen@yahoo.fr>
2015-01-10 01:24:46 +0100

    partie ajout finie

--- DeveloppementSousGit/src/menuetoptions/Ajout.java
+++ DeveloppementSousGit/src/menuetoptions/Ajout.java
@@ -1,6 +1,7 @@
 package menuetoptions;
 
 import gestiondelaffichage3d.AffichageDuModele;
+import gestiondelaffichage3d.VerifGts;
 
 import java.awt.Container;
 import java.awt.Dimension;
@@ -25,8 +26,8 @@ public class Ajout extends JFrame {
 	private static final long serialVersionUID = 1L;
 	AffichageDuModele aff;
 	final JTextField fichier;
-	final JCheckBox cbox;
-	final JCheckBox cbox1;
+	final JLabel label1;
+	final JLabel label2;
 	final JTextField tfield;
 	final JTextField tfield1;
 	final JFileChooser fc;
@@ -44,7 +45,7 @@ public class Ajout extends JFrame {
 		final JPanel pannelCharger = new JPanel();
 		fc = new JFileChooser(".");
 		charger.setPreferredSize(new Dimension(150, 27));
-		fichier = new JTextField("Veuiller chosir un fichier");
+		fichier = new JTextField("Veuiller choisir un fichier");
 		fichier.setPreferredSize(new Dimension(350, 27));
 		pannelCharger.add(fichier);
 		pannelCharger.add(charger);
@@ -52,7 +53,8 @@ public class Ajout extends JFrame {
 		charger.addActionListener(new ActionListenerKarenNumero2(this));
 
 		Container c = this.getContentPane();
-		c.setLayout(new GridLayout(12, 2, 7, 7));
+		c.setLayout(new GridLayout(8,1,9
+				,9));
 		c.add(pannelCharger);
 
 		c.add(new JLabel(
@@ -62,39 +64,20 @@ public class Ajout extends JFrame {
 
 		final JPanel contentPane = new JPanel();
 		tfield = new JTextField(15);
-		tfield.setEnabled(false);
+		label1 = new JLabel("Nom de l'objet: ");
 
-		cbox = new JCheckBox("Nom de l'objet: ", false);
 
-		ItemListener itemListener = new ItemListener() {
-			public void itemStateChanged(ItemEvent ie) {
-				tfield.setEnabled(ie.getStateChange() == ItemEvent.SELECTED);
-			}
-		};
-		cbox.addItemListener(itemListener);
 
-		contentPane.add(cbox);
+		contentPane.add(label1);
 		contentPane.add(tfield);
 
 		c.add(contentPane);
-
 		final JPanel contentPane1 = new JPanel();
-
 		tfield1 = new JTextField(15);
+		label2 = new JLabel("Auteur:    ");
 
-		tfield1.setEnabled(false);
 
-		cbox1 = new JCheckBox("Auteur:    ", false);
-
-		ItemListener itemListener1 = new ItemListener() {
-			public void itemStateChanged(ItemEvent ie) {
-				tfield1.setEnabled(ie.getStateChange() == ItemEvent.SELECTED);
-
-			}
-		};
-		cbox1.addItemListener(itemListener1);
-
-		contentPane1.add(cbox1);
+		contentPane1.add(label2);
 		contentPane1.add(tfield1);
 		c.add(contentPane1);
 
@@ -120,7 +103,7 @@ public class Ajout extends JFrame {
 
 		annuler.addActionListener(new ActionListenerKarenNumero3(this));
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setSize(640, 650);
+		this.setSize(540,400);
 		this.setResizable(false);
 		this.setVisible(true);
 
@@ -194,180 +177,171 @@ public class Ajout extends JFrame {
 		}
 
 		public void actionPerformed(ActionEvent e) {
-			if (a.fichier.getText().equals(("Veuiller chosir un fichier"))) {
-				JOptionPane.showMessageDialog(a, "Veuillez choisir un fichier",
-						"Attention", JOptionPane.WARNING_MESSAGE);
-			}
 
-			if (!a.cbox1.isSelected() || !a.cbox.isSelected()) {
+
+			if (tfield.getText().isEmpty() || tfield1.getText().isEmpty()) {
 				JOptionPane.showMessageDialog(a,
 						"Tous les champs sont obligatoire", "Attention",
 						JOptionPane.WARNING_MESSAGE);
 
-			} else if ((cbox1.isSelected() && tfield1.getText().isEmpty())
-					|| (cbox.isSelected() && tfield.getText().isEmpty())) {
-				JOptionPane.showMessageDialog(a,
-						"Un champ coche ne peut etre vide.", "Attention",
-						JOptionPane.WARNING_MESSAGE);
 			}
 
 			// sinon insertion dans la base
 
 			else {
-				if (fichier.getText().equals("Veuiller chosir un fichier")) {
+				if (fichier.getText().equals("Veuiller choisir un fichier")) {
 					JOptionPane.showMessageDialog(a,
-							"Veuillez choisr un fichier", "Attention",
+							"Veuillez choisir un fichier", "Attention",
 							JOptionPane.WARNING_MESSAGE);
 				} else {
 					// si verification gts valide sinon popup d'erreur
-					/*
-					 * VerificateurGts v=new
-					 * VerificateurGts(fc.getSelectedFile());
-					 * if(!v.FichierEntierValide()){
-					 * JOptionPane.showMessageDialog(a, "Fichier Non valide",
-					 * "Attention", JOptionPane.WARNING_MESSAGE);
-					 */
 
-					// }else{
-					// popup de confirmation
+					VerifGts v=new VerifGts(fichier.getText());
+					if(!v.GtsEstCorrect()){
+						JOptionPane.showMessageDialog(a, "Fichier Non valide",
+								"Attention", JOptionPane.WARNING_MESSAGE);
 
-					Object[] options = { "Ok,visualiser", "Annuler l'ajout" };
-					JOptionPane confirme = new JOptionPane();
-					int n = JOptionPane.showOptionDialog(a,
-							"Objet bien ajoute", "A Silly Question",
-							JOptionPane.YES_NO_OPTION,
-							JOptionPane.QUESTION_MESSAGE, null, // do not use a
-																// custom Icon
-							options, // the titles of buttons
-							options[0]); // default button title
 
-					// si on confirme l'ajout
-					if (n == JOptionPane.YES_OPTION) {
-						String ligne;
-						int cpt = 0;
-						FileReader flux;
-						BufferedReader entree = null;
-						PrintWriter sortie = null;
-						ArrayList<String> liste = new ArrayList<String>();
-						try {
+					}else{
+						// popup de confirmation
 
-							flux = new FileReader(fc.getSelectedFile());
-							System.out.println(fc.getSelectedFile());
-							entree = new BufferedReader(flux);
-							sortie = new PrintWriter("./ressources/modeles/"
-									+ tfield.getText() + ".gts");
+						Object[] options = { "Ok,visualiser", "Annuler l'ajout" };
+						JOptionPane confirme = new JOptionPane();
+						int n = JOptionPane.showOptionDialog(a,
+								"Objet bien ajoute", "A Silly Question",
+								JOptionPane.YES_NO_OPTION,
+								JOptionPane.QUESTION_MESSAGE, null, // do not use a
+								// custom Icon
+								options, // the titles of buttons
+								options[0]); // default button title
 
-							while ((ligne = entree.readLine()) != null) {
-
-								liste.add(ligne);
-								sortie.println(liste.get(cpt++));
-
-							}
-						} catch (Exception e1) {
-							System.err.println(e1.toString());
-						} finally {
+						// si on confirme l'ajout
+						if (n == JOptionPane.YES_OPTION) {
+							String ligne;
+							int cpt = 0;
+							FileReader flux;
+							BufferedReader entree = null;
+							PrintWriter sortie = null;
+							ArrayList<String> liste = new ArrayList<String>();
 							try {
-								entree.close();
-								sortie.close();
+
+								flux = new FileReader(fc.getSelectedFile());
+								System.out.println(fc.getSelectedFile());
+								entree = new BufferedReader(flux);
+								sortie = new PrintWriter("./ressources/modeles/"
+										+ tfield.getText() + ".gts");
+
+								while ((ligne = entree.readLine()) != null) {
+
+									liste.add(ligne);
+									sortie.println(liste.get(cpt++));
+
+								}
 							} catch (Exception e1) {
-								e1.printStackTrace();
-							}
-
-						}
-
-						String complexite = "";
-						FileReader flux1;
-						BufferedReader entree1 = null;
-						try {
-
-							flux1 = new FileReader("./ressources/modeles/"
-									+ tfield.getText() + ".gts");
-							entree1 = new BufferedReader(flux1);
-							String tmp = entree1.readLine();
-							int space = 0;
-
-							for (int i = 0; i < tmp.length(); ++i) {
-								if (space == 2) {
-									complexite += tmp.charAt(i);
-								} else {
-									if (Character.isWhitespace(tmp.charAt(i)))
-										space++;
+								System.err.println(e1.toString());
+							} finally {
+								try {
+									entree.close();
+									sortie.close();
+								} catch (Exception e1) {
+									e1.printStackTrace();
 								}
 
 							}
 
-							entree1.close();
-
-						} catch (Exception e1) {
-							System.err.println(e.toString());
-						} finally {
+							String complexite = "";
+							FileReader flux1;
+							BufferedReader entree1 = null;
 							try {
+
+								flux1 = new FileReader("./ressources/modeles/"
+										+ tfield.getText() + ".gts");
+								entree1 = new BufferedReader(flux1);
+								String tmp = entree1.readLine();
+								int space = 0;
+
+								for (int i = 0; i < tmp.length(); ++i) {
+									if (space == 2) {
+										complexite += tmp.charAt(i);
+									} else {
+										if (Character.isWhitespace(tmp.charAt(i)))
+											space++;
+									}
+
+								}
+
 								entree1.close();
+
 							} catch (Exception e1) {
-								e1.printStackTrace();
-							}
-
-						}
-
-						Connection c = null;
-						Statement stmt = null;
-						try {
-							Class.forName("org.sqlite.JDBC");
-							c = DriverManager
-									.getConnection("jdbc:sqlite:Database.db");
-							c.setAutoCommit(false);
-							stmt = c.createStatement();
-							String sql;
-							String name;
-							String auteur;
-
-							if (cbox.isSelected()
-									&& !tfield.getText().equals(null)
-									&& !tfield1.getText().equals(null)) {
-
-								name = tfield.getText();
-								auteur = tfield1.getText();
-								sql = "INSERT INTO OBJETS3D (NAME,AUTEUR,COMPLEXITE,LIEN) "
-										+ "VALUES ('"
-										+ name
-										+ "', '"
-										+ auteur
-										+ "', '"
-										+ Integer.parseInt(complexite)
-										+ "', '" + fichier.getText() + "' );";
-								stmt.executeUpdate(sql);
-								a.setEnabled(false);
-								confirme.setVisible(false);
-								aff.nouvelOnglet(tfield.getText());
-								a.dispose();
-								AffichageDuModele.modifierInfos
-										.setEnabled(true);
+								System.err.println(e.toString());
+							} finally {
+								try {
+									entree1.close();
+								} catch (Exception e1) {
+									e1.printStackTrace();
+								}
 
 							}
 
-						} catch (Exception e1) {
-							System.err.println(e1.getClass().getName() + ": "
-									+ e1.getMessage());
-							System.exit(0);
-
-						} finally {
-							// fermeture de connection
+							Connection c = null;
+							Statement stmt = null;
 							try {
-								stmt.close();
-								c.commit();
-								c.close();
-							} catch (SQLException e1) {
-								e1.printStackTrace();
+								Class.forName("org.sqlite.JDBC");
+								c = DriverManager
+										.getConnection("jdbc:sqlite:Database.db");
+								c.setAutoCommit(false);
+								stmt = c.createStatement();
+								String sql;
+								String name;
+								String auteur;
+
+								if (!tfield.getText().equals(null)
+										&& !tfield1.getText().equals(null)) {
+
+									name = tfield.getText();
+									auteur = tfield1.getText();
+									sql = "INSERT INTO OBJETS3D (NAME,AUTEUR,COMPLEXITE,LIEN) "
+											+ "VALUES ('"
+											+ name
+											+ "', '"
+											+ auteur
+											+ "', '"
+											+ Integer.parseInt(complexite)
+											+ "', '" + fichier.getText() + "' );";
+									stmt.executeUpdate(sql);
+									a.setEnabled(false);
+									confirme.setVisible(false);
+									aff.nouvelOnglet(tfield.getText());
+									a.dispose();
+									AffichageDuModele.modifierInfos
+									.setEnabled(true);
+
+								}
+
+							} catch (Exception e1) {
+								System.err.println(e1.getClass().getName() + ": "
+										+ e1.getMessage());
+								System.exit(0);
+
+							} finally {
+								// fermeture de connection
+								try {
+									stmt.close();
+									c.commit();
+									c.close();
+								} catch (SQLException e1) {
+									e1.printStackTrace();
+								}
+
 							}
 
+							// si on annule l'ajout
+						} else if (n == JOptionPane.NO_OPTION) {
+							confirme.setVisible(false);
+							JOptionPane.showMessageDialog(a, "Ajout annule", " ",
+									JOptionPane.WARNING_MESSAGE);
+							a.dispose();
 						}
-
-						// si on annule l'ajout
-					} else if (n == JOptionPane.NO_OPTION) {
-						confirme.setVisible(false);
-						JOptionPane.showMessageDialog(a, "Ajout annule", " ",
-								JOptionPane.WARNING_MESSAGE);
-						a.dispose();
 
 					}
 				}
