860f4c85d5a3248a30f72499726ae3c1cb32c172
damien-lepeltier <damien.lepeltier@etudiant.univ-lille1.fr>
2015-01-11 23:50:20 +0100

    Classe Test segment fait au minimal

--- DeveloppementSousGit/src/test/SegmentTest.java
+++ DeveloppementSousGit/src/test/SegmentTest.java
@@ -2,33 +2,36 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
+
+import javax.swing.text.Segment;
+
 import org.junit.Test;
 
 public class SegmentTest {
-
-	@Test
-	public void testSegment() {
-		fail("Not yet implemented");
-	}
+		gestiondelaffichage3d.Point p1 = new gestiondelaffichage3d.Point(1.1,2.2,3.3,4);
+		gestiondelaffichage3d.Point p2 = new gestiondelaffichage3d.Point(1.1,2.2,3.3,4);
+		gestiondelaffichage3d.Segment s1 = new gestiondelaffichage3d.Segment(p1, p2, 5);
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		assertEquals(p1+" "+p2,s1.toString());
 	}
 
 	@Test
 	public void testGetNumero() {
-		fail("Not yet implemented");
+
+		assertEquals(5,s1.getNumero());
 	}
 
 	@Test
 	public void testGetP1() {
-		fail("Not yet implemented");
+		assertEquals(p1,s1.getP1());
 	}
 
 	@Test
 	public void testGetP2() {
-		fail("Not yet implemented");
+		assertEquals(p2,s1.getP2());
 	}
 
 }
